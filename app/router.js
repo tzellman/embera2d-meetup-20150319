9b126da8525aaf790c20f270bb29d85c4f9305a4
Tom Zellman <tzellman@gmail.com>
2015-04-12 00:55:40 -0400

    ember-cli -> 0.2.3; ember-slide-show -> 0.0.4

--- app/router.js
+++ app/router.js
@@ -2,11 +2,8 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-    location: config.locationType
+  location: config.locationType
 });
 
-Router.map(function () {
+export default Router.map(function() {
 });
-
-export default
-Router;
