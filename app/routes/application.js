b40713ba20a53a9f0bda53dfe3737867f08429d2
Tom Zellman <tzellman@gmail.com>
2015-04-16 07:05:12 -0400

    slides for 20150416 meetup

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,24 +3,12 @@ import Ember from 'ember';
 var SLIDES = [
     "welcome",
     "who-we-are",
-    "upcoming-topics",
+    "announcements",
     "sponsors",
+    "upcoming-topics",
     "upcoming-events",
-    "intros",
-    "talk-intro",
-    "talk-outline",
-    "talk-what",
-    "talk-what2",
-    "talk-why",
-    "talk-why2",
-    "talk-using",
-    "talk-using2",
-    "talk-app",
-    "talk-app1",
-    "talk-app2",
-    "talk-app3",
-    "talk-addon",
-    "talk-end"
+    "other-events",
+    "intros"
 ];
 
 
